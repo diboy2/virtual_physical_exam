3e89ca93cb5b8f56446e9850918d76f6cbd9bc0e
Adrian Cagaanan <acagaana@villanova.edu>
2022-08-29 18:08:41 -0400

    Unfinished video recognition

--- run.sh
+++ run.sh
@@ -9,10 +9,11 @@ fuser -k 9998/tcp
 fuser -k 9999/tcp
 
 python3 -m storeDailyMetrics &
+python3 -m storeImage &
+python3 -m storeVideo &
 python3 -m storeMedicalHistory &
 python3 -m storePatientConcerns &
 python3 -m storePhysicianNotes &
-python3 -m storeImage &
 python3 -m testImageRecognition &
 python3 -m testTextRecognition &
 wait
--- testVideoRecognition.py
+++ testVideoRecognition.py
@@ -17,78 +17,22 @@ def get_joined_labels(videoUri):
 	client = videointelligence.VideoIntelligenceServiceClient()
 	features = [videointelligence.Feature.LABEL_DETECTION]
 
-	# The name of the image file to annotate
+	# The name of the video file to annotate
 	response = client.annotate_video(
 		request= { "features": features, 'input_uri': videoUri }
 	)
-	print(f"Finished video recognition of video uri: {videoUri}.")
 
 	result = response.result(timeout=90)
-	print("\nFinished processing.")
-	# Process video/segment level label annotations
+	print(f"Finished video recognition of video uri: {videoUri}.")
+
 	segment_labels = result.annotation_results[0].segment_label_annotations
+	labels = []
+
 	for i, segment_label in enumerate(segment_labels):
-		print("Video label description: {}".format(segment_label.entity.description))
-		for category_entity in segment_label.category_entities:
-			print(
-				"\tLabel category description: {}".format(category_entity.description)
-			)
+		categories = map(lambda category: category.description,segment_label.category_entities )
+		labels.append({ "label": segment_label.entity.description, "categories": categories})
 
-		for i, segment in enumerate(segment_label.segments):
-			start_time = (
-				segment.segment.start_time_offset.seconds
-				+ segment.segment.start_time_offset.microseconds / 1e6
-			)
-			end_time = (
-				segment.segment.end_time_offset.seconds
-				+ segment.segment.end_time_offset.microseconds / 1e6
-			)
-			positions = "{}s to {}s".format(start_time, end_time)
-			confidence = segment.confidence
-			print("\tSegment {}: {}".format(i, positions))
-			print("\tConfidence: {}".format(confidence))
-		print("\n")
-	descriptions = list(map(lambda label: label.description, response.label_annotations))
-	return ' '.join(descriptions)
-"""Detect labels given a file path."""
-video_client = videointelligence.VideoIntelligenceServiceClient()
-features = [videointelligence.Feature.LABEL_DETECTION]
-
-with io.open(path, "rb") as movie:
-    input_content = movie.read()
-
-operation = video_client.annotate_video(
-    request={"features": features, "input_content": input_content}
-)
-print("\nProcessing video for label annotations:")
-
-result = operation.result(timeout=90)
-print("\nFinished processing.")
-
-# Process video/segment level label annotations
-segment_labels = result.annotation_results[0].segment_label_annotations
-for i, segment_label in enumerate(segment_labels):
-    print("Video label description: {}".format(segment_label.entity.description))
-    for category_entity in segment_label.category_entities:
-        print(
-            "\tLabel category description: {}".format(category_entity.description)
-        )
-
-    for i, segment in enumerate(segment_label.segments):
-        start_time = (
-            segment.segment.start_time_offset.seconds
-            + segment.segment.start_time_offset.microseconds / 1e6
-        )
-        end_time = (
-            segment.segment.end_time_offset.seconds
-            + segment.segment.end_time_offset.microseconds / 1e6
-        )
-        positions = "{}s to {}s".format(start_time, end_time)
-        confidence = segment.confidence
-        print("\tSegment {}: {}".format(i, positions))
-        print("\tConfidence: {}".format(confidence))
-    print("\n")
-
+	return ' '.join(labels)
 
 def doit():
 	while True:	
